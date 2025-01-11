167edfb9ab9bd04bd261a998f39efc4f1141065b
duckgii <shinyonggi@naver.com>
2025-01-11 18:52:52 +0900

    REFACTOR : wp db reset 하지 않도록 변경

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -12,37 +12,37 @@ else
 fi
 
 # wp-config.php 생성 (이미 존재하는지 확인)
-#if [ ! -f wp-config.php ]; then
-echo "Creating wp-config.php..."
-wp config create \
-  --dbname=wordpress_db \
-  --dbuser=my_user \
-  --dbpass=my_password \
-  --dbhost=mariadb \
-  --allow-root
-#else
-#  echo "wp-config.php already exists."
-#fi
+if [ ! -f wp-config.php ]; then
+	echo "Creating wp-config.php..."
+	wp config create \
+	  --dbname=wordpress_db \
+	  --dbuser=my_user \
+	  --dbpass=my_password \
+	  --dbhost=mariadb \
+	  --allow-root
+else
+  echo "wp-config.php already exists."
+fi
 
 # WordPress 설치 (이미 설치되어 있는지 확인)
-#if ! wp core is-installed --allow-root; then
-#  echo "Installing WordPress..."
+if ! wp core is-installed --allow-root; then
+  echo "Installing WordPress..."
+  #wp db reset --yes --allow-root
+  wp core install \
+    --url=localhost \
+    --title="YEOSHIN" \
+    --admin_user=wpcli \
+    --admin_password=wpcli \
+    --admin_email=info@wp-cli.org \
+    --allow-root --debug
+
+else
+  echo "WordPress is already installed."
+
+fi
 
 
-wp db reset --yes --allow-root
-wp core install \
-  --url=localhost \
-  --title="WP-CLI222" \
-  --admin_user=wpcli \
-  --admin_password=wpcli \
-  --admin_email=info@wp-cli.org \
-  --allow-root --debug
 
-
-#else
-#  echo "WordPress is already installed."
-
-#fi
 
 # 컨테이너가 실행 상태를 유지하도록
 exec "$@"
